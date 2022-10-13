3c72ec1384aaadca5680e0144e88f5c6581dbde9
Erifu <Erifu@LAPTOP-CROM4B8A>
2022-10-13 17:30:15 +0200

    13/10

--- src/personnages/Chef.java
+++ src/personnages/Chef.java
@@ -6,11 +6,12 @@ public class Chef {
 	private int effetPotion = 1;
 	private Village village;
 	
-	public Chef(String nom, int force, Village village ) {
+	public Chef(String nom, int force, Village village) {
 		this.nom = nom;
 		this.force = force;
 		this.effetPotion = effetPotion;
 		this.village = village;
+
 		
 	}
 	
---
+++ src/personnages/Village.java
@@ -0,0 +1,18 @@
+package personnages;
+
+public class Village {
+	private String nom;
+	private Chef chef;
+	
+	public Village(String nom) {
+	this.nom = nom;
+	}
+	
+	public void setChef(Chef chef) {
+	this.chef = chef;
+	}
+	
+	public String getNom() {
+	return nom;
+	}
+}
